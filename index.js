5af3a687d48d513a700411ff226bcf138e3b16c8
mohammad <mohammadalizafar986@gmail.com>
2023-12-03 01:25:36 -0600

    fixed getting length of object

--- index.js
+++ index.js
@@ -90,7 +90,8 @@ function move(gameState) {
   const myBody = gameState.you.body;
   console.log("Body: ", myBody);
   console.log("Body part count: ", Object.keys(myBody));
-  for (let i = 0; i < Object.keys(myBody); i++) {
+
+  for (let i = 0; i < Object.keys(myBody).length(); i++) {
     console.log("loop count: ", i);
     if (myHead.y + 1 == myBody[i].y) {
       isMoveSafe.up = false;
